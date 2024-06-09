f2707968a4e9cc40d0962c138537dd4729cee7e1
Anton <rodrigescouy25@gmail.com>
2024-06-09 14:22:28 +0300

    aside for mobile

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-document.querySelector(".button-menu").addEventListener("click", function () {
-  document.querySelector(".sidebar").classList.toggle("active");
+document.addEventListener("DOMContentLoaded", function () {
+  const sidebar = document.querySelector(".sidebar");
+  const menuButton = document.querySelector(".button-menu");
+  const closeButton = document.querySelector(".button__close");
+
+  // Toggle sidebar on menu button click
+  menuButton.addEventListener("click", function () {
+    sidebar.classList.toggle("sidebar--active");
+  });
+
+  // Hide sidebar on close button click
+  closeButton.addEventListener("click", function () {
+    sidebar.classList.remove("sidebar--active");
+  });
 });
